800b2b67d57c03c3f71de1febc0b36aeafd23dd1
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-22 16:41:55 -0300

    flag_keys.sh v1.0

---
+++ mod3/flags_keys.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#
+# usersLists - Extract users from /etc/passwd
+#
+# Site:          https://url.com
+# Autor:         Marcos Guillermo
+# Maintenance:   Marcos Guillermo
+# 
+# ------------------------------------------------------------------------------------#
+# This program will extract users from the /etc/passwd path, with the possibility to put
+# in uppercase and alphabetical order
+#
+# Exemples:
+#	$./usersLists.sh -s -m
+#	In this exemple, the list will be alphabetized and uppercase(respectively).
+# ------------------------------------------------------------------------------------#
+# Change log:
+# 
+#
+# 	v1.0 22/08/2021, Marcos Guillermo:
+#  - 
+#
+# ------------------------------------------------------------------------------------#
+# Test in:
+# 	bash x.x.xx
+# ------------------------------------------------------------------------------------#
+# Acknowledgments:
+#
+# 	Matheus Müller - Taught me UNIX commands and awakened my knowledge of DevOps.
+# 	Jonatas Ferreira - Introduced me to what would be DevOps
+# ------------------------------------------------------------------------------------#
+#
+#
+#--------------------------------------Variables----------------------------------------------#
+
+USERS="$(cat /etc/passwd | cut -d : -f 1)"
+USER_MESSENGER="
+	$0 - [OPTIONS]
+
+	-h - Help Menu
+	-v - Version
+	-s - Exit order
+"
+VERSION="1.0v"
+
+# ------------------------------------------------------------------------------------#
+#
+# --------------------------------------Tests----------------------------------------------
+#
+#
+#
+# ------------------------------------------------------------------------------------#
+#
+# --------------------------------------Functions----------------------------------------------#
+#
+#
+# ------------------------------------------------------------------------------------#
+#
+# --------------------------------------Execution----------------------------------------------#
+if [ "$1" = "-h" ]; then
+	echo "$USER_MESSENGER" && exit 0
+fi
+
+if [ "$1" = "-v" ]; then
+	echo "$VERSION" && exit 0
+fi
+
+if [ "$1" = "-s" ]; then
+	echo "$USERS" | sort && exit 0
+fi
+
+
+
+echo "$USERS"
+
+
+
+# ------------------------------------------------------------------------------------#
+
+
