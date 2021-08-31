a1ce201445c4efa9f21fa3a98bfce4dc0a5d70a4
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-31 08:58:30 -0300

    Error handling

---
+++ mod9/user_system.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# programName.sb - Description of program
+#
+# Site:          https://url.com
+# Autor:         Marcos Guillermo
+# Maintenance:   Marcos Guillermo
+# 
+# ------------------------------------------------------------------------------------#
+# This program will have the description based on an Api 
+#
+# Exemples:
+#	$./Header.sh -d 1
+#	In this example the script will be run in debug mode level 1
+# ------------------------------------------------------------------------------------#
+# Historic/Change log:
+# 
+#
+# 	v1.0 18/08/2021, Autor of change:
+#	   - What has changed?
+#	v1.1 19/09/2021, Autor of change:
+#	   - What has changed?
+#
+# ------------------------------------------------------------------------------------#
+# Test in:
+# 	bash x.x.xx
+# ------------------------------------------------------------------------------------#
+# Acknowledgments:
+#
+# 	Matheus Müller - Taught me UNIX commands and awakened my knowledge of DevOps.
+# 	Jonatas Ferreira - Introduced me to what would be DevOps
+# ------------------------------------------------------------------------------------#
+#
+#
+#--------------------------------------Variables----------------------------------------------#
+DATA_BASE_FILE="data_base.txt"
+
+GREEN="\033[32;1m"
+RED="\033[31;1m"
+# ------------------------------------------------------------------------------------#
+#
+# --------------------------------------Tests-------------------------------------------- 
+[ ! -e "$DATA_BASE_FILE" ] && echo "ERROR. File does exist"                    && exit 1
+[ ! -r "$DATA_BASE_FILE" ] && echo "ERROR. File don't have a read permisson"   && exit 1
+[ ! -w "$DATA_BASE_FILE" ] && echo "ERROR. File don't have writing permission" && exit 1
+
+
+# ------------------------------------------------------------------------------------#
+
+# --------------------------------------Functions----------------------------------------------#
+#
+#
+# ------------------------------------------------------------------------------------#
+#
+# --------------------------------------Execution----------------------------------------------#
+#
+#
+#
+# ------------------------------------------------------------------------------------#
+#
+
+
+
+
+
+
+
+
+
+#
+
+3
+#
+
